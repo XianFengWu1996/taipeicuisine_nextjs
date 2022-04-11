967173d170a742b2754820a00231d667db9ad0f1
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-04-11 02:06:50 -0400

    change updatePaymentIntent

--- utils/functions/payment.ts
+++ utils/functions/payment.ts
@@ -69,45 +69,44 @@ export const handlePayWithIntent = async (val: IPayWithIntent) => {
             headers: { 'Authorization': `Bearer ${await token()}`},
             data: {
                 total: val.cart.total,
-                future_use: val.future_use
             }
         })
 
-        // confirm the payment with stripe
-        const { error } = await val.stripe.confirmPayment({
-            elements: val.elements,
-            redirect: 'if_required',
-            confirmParams: {
-                return_url: 'http://localhost:3000/order/payment',
-            }
-        });
+        // // confirm the payment with stripe
+        // const { error } = await val.stripe.confirmPayment({
+        //     elements: val.elements,
+        //     redirect: 'if_required',
+        //     confirmParams: {
+        //         return_url: 'http://localhost:3000/order/payment',
+        //     }
+        // });
     
-        if(error){
-            if (error.type === "card_error" || error.type === "validation_error") {
-                throw new Error(error.message ?? "An unexpected error occured.")
-            }
-        }
+        // if(error){
+        //     if (error.type === "card_error" || error.type === "validation_error") {
+        //         throw new Error(error.message ?? "An unexpected error occured.")
+        //     }
+        // }
         
 
-        await axios({
-            method: 'POST',
-            url: `${process.env.NEXT_PUBLIC_CF_URL}/payment/place_online_order`,
-            headers: { 'Authorization': `Bearer ${await token()}`},
-            data: { 
-                cart: val.cart,
-                customer: {
-                    name: val.customer.name,
-                    phone: val.customer.phone,
-                    address: val.customer.address
-                },
-                payment_intent: update_result.data.payment_intent,
-                is_new: val.is_new
-            }
-        })
+        // await axios({
+        //     method: 'POST',
+        //     url: `${process.env.NEXT_PUBLIC_CF_URL}/payment/place_online_order`,
+        //     headers: { 'Authorization': `Bearer ${await token()}`},
+        //     data: { 
+        //         cart: val.cart,
+        //         customer: {
+        //             name: val.customer.name,
+        //             phone: val.customer.phone,
+        //             address: val.customer.address
+        //         },
+        //         payment_intent: update_result.data.payment_intent,
+        //         is_new: val.is_new
+        //     }
+        // })
 
-        // // if the order is placed, route to confirmation page
-        Router.push('/order/confirmation');
-        store.dispatch(orderComplete());
+        // // // if the order is placed, route to confirmation page
+        // Router.push('/order/confirmation');
+        // store.dispatch(orderComplete());
     } catch (error) {
         handleCatchError((error as Error), 'Failed to confirm payment')
     }
