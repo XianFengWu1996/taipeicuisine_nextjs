9a75d48e6f7edd13027979a19b0f8bfabbabad66
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-04-21 02:52:49 -0400

    create lunchOption component

--- components/dialogs/publicMenuDialog/publicMenuDialog.tsx
+++ components/dialogs/publicMenuDialog/publicMenuDialog.tsx
@@ -164,38 +164,21 @@ export const PublicMenuDialog = (props: IPublicMenuDialogProps) => {
                             setComments(e.target.value)
                         }}/>
 
-
+                        <LunchOption 
+                            lunchOption={lunchOption}
+                            handleOnOptionChange={handleOnLunchOptionChange}
+                            handleOnSubChange={(e) => {
+                                setLunchOption({
+                                    ...lunchOption,
+                                    sub: e.target.checked,
+                                    no_soup: e.target.checked ? false : e.target.checked,
+                                })
+                            }}
+                        />
 
                     
            
-                    <FormGroup>
-                        <FormControlLabel 
-                            control={<Checkbox 
-                                name="sub"
-                                checked={lunchOption.sub}
-                                onChange={(e) => {
-                                    setLunchOption({
-                                        ...lunchOption,
-                                        sub: e.target.checked,
-                                        no_soup: e.target.checked ? false : e.target.checked,
-                                    })
-                                }}
-                            />} label="Substitute Hot&Sour Soup" />
-                        <FormControlLabel  
-                            control={<Checkbox 
-                                name="no_soup"
-                                checked={lunchOption.no_soup}
-                                onChange={handleOnLunchOptionChange}
-                                disabled={lunchOption.sub}
-                            />} label="No Soup"/>
-                        <FormControlLabel  
-                            control={<Checkbox 
-                                name="no_rice"
-                                checked={lunchOption.no_rice}
-                                onChange={handleOnLunchOptionChange}
-                            />} label="No Rice"/>
-
-                    </FormGroup>
+                    
                 </div>
 
 
@@ -217,4 +200,37 @@ export const PublicMenuDialog = (props: IPublicMenuDialogProps) => {
     </Dialog>
 }
 
+interface ILunchOptionProps {
+    lunchOption: {
+        sub: boolean,
+        no_soup:boolean,
+        no_rice:boolean,
+    },
+    handleOnOptionChange: (e: ChangeEvent<HTMLInputElement>) => void,
+    handleOnSubChange: (e: ChangeEvent<HTMLInputElement>) => void
+}
 
+export const LunchOption = (_: ILunchOptionProps) => {
+return <FormGroup>
+                        <FormControlLabel 
+                            control={<Checkbox 
+                                name="sub"
+                                checked={_.lunchOption.sub}
+                                onChange={_.handleOnSubChange}
+                            />} label="Substitute Hot&Sour Soup" />
+                        <FormControlLabel  
+                            control={<Checkbox 
+                                name="no_soup"
+                                checked={_.lunchOption.no_soup}
+                                onChange={_.handleOnOptionChange}
+                                disabled={_.lunchOption.sub}
+                            />} label="No Soup"/>
+                        <FormControlLabel  
+                            control={<Checkbox 
+                                name="no_rice"
+                                checked={_.lunchOption.no_rice}
+                                onChange={_.handleOnOptionChange}
+                            />} label="No Rice"/>
+
+                    </FormGroup>    
+}
