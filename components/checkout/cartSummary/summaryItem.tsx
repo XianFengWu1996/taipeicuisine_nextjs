9e6929c24ce81c93159e35f2020fc7263a660c42
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-04-25 18:58:49 -0400

    replace summary item with card

--- components/checkout/cartSummary/summaryItem.tsx
+++ components/checkout/cartSummary/summaryItem.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material"
+import { Card, CardContent, Typography } from "@mui/material"
 import { blue } from "@mui/material/colors"
 import { styled } from "@mui/system"
 import { isEmpty } from "lodash"
@@ -42,32 +42,33 @@ const CountPriceText = styled(Typography)(({ theme }) => ({
 export const SummaryItem = ({ item } : ISummaryItemProps)  => {
     const opt = item.lunchOption;
 
-    return  <div style={{ display: 'flex', marginBottom: '3px'}}>
-    <div style={{ flex: 1}}>
-        <CountPriceText>x {item.quantity}</CountPriceText>
-    </div>
-    <div style={{ flex: 6, marginRight: '5px'}}>
-        <NameText>{item.dish.label_id}. {item.dish.en_name} {item.dish.ch_name}</NameText>
-        {
-            !isEmpty(item.option) && <OptionText>Option: {item.option.en_name} {item.option.ch_name} {item.option.spicy ? <GoFlame color="red"/>: null}</OptionText> 
-        }
+    return  <Card sx={{ my: 1.5}}>
+        <CardContent sx={{ display: 'flex'}}>
+            <div style={{ flex: 1}}>
+                <CountPriceText>x {item.quantity}</CountPriceText>
+            </div>
+            <div style={{ flex: 6, marginRight: '5px'}}>
+                <NameText> {item.dish.label_id}. {item.dish.en_name} {item.dish.ch_name} {isEmpty(item.option) && (item.dish.is_spicy &&  <GoFlame color="red"/>) }</NameText>
+                {
+                    !isEmpty(item.option) && <OptionText>Option: {item.option.en_name} {item.option.ch_name} {item.option.spicy ? <GoFlame color="red"/>: null}</OptionText> 
+                }
 
-        {
-            (opt && (opt.sub || opt.no_rice || opt.no_soup) ) && <Typography sx={{fontSize: '12px', fontWeight: 600, fontStyle: 'italic', color: blue[600] }}>
-                    Lunch Option: 
-                {opt.sub && 'Hot&Sour soup, '}
-                {opt.no_rice && 'No Rice, '} 
-                {opt.no_soup && 'No Soup '}
-            </Typography>
-        }
+                {
+                    (opt && (opt.sub || opt.no_rice || opt.no_soup) ) && <Typography sx={{fontSize: '12px', fontWeight: 600, fontStyle: 'italic', color: blue[600] }}>
+                            Lunch Option: 
+                        {opt.sub && 'Hot&Sour soup, '}
+                        {opt.no_rice && 'No Rice, '} 
+                        {opt.no_soup && 'No Soup '}
+                    </Typography>
+                }
 
-        {
-            !isEmpty(item.comment) && <CommentText>Comments: {item.comment}</CommentText>
-        }
-    </div>
-    <div style={{ flex: 1, display: 'flex', justifyContent: 'end'}}>
-        <CountPriceText>${item.total.toFixed(2)}</CountPriceText>
-    </div>
-
-</div>
+                {
+                    !isEmpty(item.comment) && <CommentText>Comments: {item.comment}</CommentText>
+                }
+            </div>
+            <div style={{ flex: 1, display: 'flex', justifyContent: 'end'}}>
+                <CountPriceText>${item.total.toFixed(2)}</CountPriceText>
+            </div>
+        </CardContent>
+    </Card>
 }
