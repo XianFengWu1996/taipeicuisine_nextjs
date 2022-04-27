23d5d470383152d381143bf04dc222430711ff1d
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-04-27 00:31:42 -0400

    finish basic confirmation page

--- pages/order/confirmation.tsx
+++ pages/order/confirmation.tsx
@@ -1,68 +1,18 @@
-import { Button } from "@mui/material";
-import axios from "axios";
-import { onAuthStateChanged } from "firebase/auth";
-import { isString } from "lodash";
-import Router, { useRouter } from "next/router";
-import { useEffect } from "react"
-import { orderComplete } from "../../store/slice/cartSlice";
-import { useAppDispatch, useAppSelector } from "../../store/store";
-import { handleCatchError } from "../../utils/errors/custom";
-import { fbAuth } from "../../utils/functions/auth";
+import { Button, Typography } from "@mui/material";
+import Router from "next/router";
+import { BsPatchCheck } from "react-icons/bs";
+
 
 export default function Confirmation () {
-    const dispatch = useAppDispatch();
-    const router = useRouter()
-
-    const cartState = useAppSelector(state => state.cart);
-    const customerState = useAppSelector(state => state.customer);
-
-    // useEffect(() => {
-    //     if(router.isReady){
-    //         if(cartState.cart.length < 0) return;
-
-    //         console.log('ran')
-
-    //         onAuthStateChanged(fbAuth, async user => {
-    //             if(!user) return;
-
-    //             if(!isString(router.query.payment_intent) || !isString(router.query.redirect_status)){
-    //                 // error message or something
-    //             }
-    
-    //             // if payment is successful
-    //             if(router.query.redirect_status === 'succeeded'){
-    //                    // submit the order our backend
-    //                     try {
-    //                         await axios({
-    //                             method: 'POST',
-    //                             url: `${process.env.NEXT_PUBLIC_CF_URL}/payment/place_online_order`,
-    //                             headers: { 'Authorization': `Bearer ${await user.getIdToken()}`},
-    //                             data: { 
-    //                                 cart: cartState,
-    //                                 customer: {
-    //                                     name: customerState.name,
-    //                                     phone: customerState.phone,
-    //                                     address: customerState.address
-    //                                 },
-    //                                 payment_intent: router.query.payment_intent,
-    //                                 is_new: router.query.is_new ? true : false
-    //                             }
-    //                         })
-    //                         dispatch(orderComplete())
-    //                     } catch (error) {
-    //                         handleCatchError((error as Error), 'Failed to place order')
-    //                     }
-    //             }
-               
-    //         })
-    //     }
-    // }, [router.isReady])
-
-    return <>
-        confirmation
-
-        <Button onClick={() => {
-            Router.push('/order')
-        }}>Return to Home</Button>
-    </>
+    return <div style={{ height: '100vh'}}>
+        <div style={{ display: 'flex', flexDirection: 'column',justifyContent: 'center', alignItems:'center', height: 'inherit'}}>
+            <div><BsPatchCheck size={100} color="#7FFFD4"/></div>
+            <Typography variant="h3">Order Confirmation</Typography>
+            <Typography>Your order has been placed. You will receive a email confirmation in a few minutes. </Typography>
+            <Button onClick={() => {
+                Router.push('/order')
+            }}>Return to Home</Button>
+        </div>
+     
+    </div>
 }
