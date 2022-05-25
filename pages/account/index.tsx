1d84c2c97f2823999e3d5adbe4dff8decc73f6e0
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-05-25 00:39:50 -0400

    add icons to account links

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -1,6 +1,10 @@
-import { Typography } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import Link from "next/link"
 import Router from "next/router"
+import { AiOutlineUser, AiOutlineWallet } from "react-icons/ai"
+import { BsCoin } from "react-icons/bs"
+import { FiSettings } from "react-icons/fi"
+import { IoReceiptOutline } from 'react-icons/io5'
 import { AccountRelatedPage } from "../../components/account"
 import { PublicAppBar } from "../../components/appbar/appbar"
 import { OrderHistory } from "../../components/history"
@@ -11,10 +15,35 @@ export default function AccountPage() {
         <PublicAppBar />
         <div style={{ display: 'flex'}}>
             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '40px', width: '400px'}}>
-                    <Link href='/account?redirect=order' passHref><Typography sx={{ fontSize: 25, mb: 2}}>Order</Typography></Link>
-                    <Link href='/account?redirect=reward' passHref><Typography sx={{ fontSize: 25, mb: 2}}>Reward</Typography></Link>
-                    <Link href='/account?redirect=account' passHref><Typography sx={{ fontSize: 25, mb: 2}}>Account</Typography></Link>
-                    <Link href='/account?redirect=setting' passHref><Typography sx={{ fontSize: 25, mb: 2}}>Setting</Typography></Link>
+                    <Link href='/account?redirect=order' passHref>
+                    <Box sx={{ display: 'flex', alignItems: 'center', mb:2, justifyContent: 'space-between', width: '160px'}}>
+                            <IoReceiptOutline size={25}/>
+                            <Typography sx={{ fontSize: 25, ml:2}}>Order</Typography>
+                        </Box>      
+                    </Link>
+                    <Link href='/account?redirect=reward' passHref>
+                    <Box sx={{ display: 'flex', alignItems: 'center', mb:2, justifyContent: 'space-between', width: '160px'}}>
+                            <BsCoin size={25}/>
+                            <Typography sx={{ fontSize: 25, ml:2}}>Reward</Typography>
+                        </Box>  
+                    </Link>
+                    <Link href='/account?redirect=wallet' passHref>
+                        <Box sx={{ display: 'flex', alignItems: 'center', mb:2, justifyContent: 'space-between', width: '160px'}}>
+                            <AiOutlineWallet size={25}/>
+                            <Typography sx={{ fontSize: 25, ml:2}}>Wallet</Typography>
+                        </Box></Link>
+                    <Link href='/account?redirect=account' passHref>
+                    <Box sx={{ display: 'flex', alignItems: 'center', mb:2, justifyContent: 'space-between', width: '160px'}}>
+                            <AiOutlineUser size={25}/>
+                            <Typography sx={{ fontSize: 25, ml:2}}>Account</Typography>
+                        </Box> 
+                    </Link>
+                    <Link href='/account?redirect=setting' passHref>
+                    <Box sx={{ display: 'flex', alignItems: 'center', mb:2, justifyContent: 'space-between', width: '160px'}}>
+                            <FiSettings size={25}/>
+                            <Typography sx={{ fontSize: 25, ml:2}}>Setting</Typography>
+                        </Box> 
+                    </Link>
                 </div>
 
         <div style={{ flex: 1}}>
