f7b8243768c9b8718ce23ee03d8d46c93d574783
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-06-03 02:24:38 -0400

    finish handling change password

--- components/account/changePassword.tsx
+++ components/account/changePassword.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField, Typography } from "@mui/material"
+import { Button, FormControl, TextField, Typography } from "@mui/material"
 import { useState } from "react"
 import { updatePassword } from 'firebase/auth'
 import { fbAuth } from "../../utils/functions/auth";
@@ -6,20 +6,48 @@ import { FirebaseError } from "firebase/app";
 import { useAppDispatch } from "../../store/store";
 import { setShowLoginDialog } from "../../store/slice/settingSlice";
 import snackbar from "../snackbar";
+import { Box } from "@mui/system";
+import { isEmpty } from "lodash";
 
 export const AccountChangePassword = () => {
     const [new_password, setNewPassword] = useState<string>('');
     const [confirm, setConfirm] = useState<string>('');
 
+    const [show_password, setShowPassword] = useState(false);
+
     const [need_auth, setNeedAuth] = useState<boolean>(false);
+    const [error, setError]= useState<string>('');
 
     const dispatch = useAppDispatch();
 
+    const checkForStrongPassword = (password: string) => {
+        let regularExpression = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
+        return regularExpression.test(password);
+    } 
+
     const handleChangePassword = async () => {
         try {
-            if((new_password === confirm) && fbAuth.currentUser){
-                await updatePassword(fbAuth.currentUser, new_password)
+            setError('');
+            if(isEmpty(new_password) ||  isEmpty(confirm)){
+                return setError('Please fill out the required fields')
             }
+
+            if(new_password !== confirm){
+                return setError('The password does not match');
+            }
+
+            if(!checkForStrongPassword(new_password)){
+                return setError('Password must have at least 8 character, and must contain at least an uppercase letter, a lowercase letter, a number, and a symbol (!@#$%^&*)');
+            }
+
+            await updatePassword(fbAuth.currentUser!, new_password)
+            snackbar.success('Password has been updated')
+            setNewPassword('')
+            setConfirm('')
+            setError('');
+            setShowPassword(false);
+            setNeedAuth(false);
+            
         } catch (error) {
             if((error as Error).name === 'FirebaseError' && (error as FirebaseError).code === 'auth/requires-recent-login'){
                 setNeedAuth(true);
@@ -33,29 +61,49 @@ export const AccountChangePassword = () => {
         dispatch(setShowLoginDialog(true)); // show the login dialog 
     }
 
-    return <div style={{  marginTop: '20px', width: '400px'}}>
-        <Typography sx={{ fontSize: 16, fontWeight: 600, mr: 2}}>Change Password</Typography>
+    return <div style={{  width: '400px'}}>
+        <Box sx={{ display: 'flex', my: 1}}>
+            <Typography sx={{ fontSize: 16, fontWeight: 600, mr: 2}}>Change Password</Typography>
+            <Button 
+                variant='text' 
+                size='small' 
+                sx={{ fontSize: 10}}
+                onClick={() =>  setShowPassword(!show_password)}
+            >Show Password</Button>
+        </Box>
+        
         <div>
-            <TextField 
-                value={new_password}
-                label={'New Password'}
-                type={'password'}
-                variant='outlined'
-                size='small'
-                fullWidth
-                sx={{ display: 'block', my: 1}}
-                onChange={(e) => {
-                    setNewPassword(e.target.value)
-                }}
-            />
+            <FormControl sx={{ display: 'flex'}}>
+
+           <div>
+                <TextField 
+                    id="new_password"
+                    value={new_password}
+                    label={'New Password'}
+                    type={show_password ? 'text': 'password'}
+                    size='small'
+                    required
+                    onChange={(e) => {
+                        setNewPassword(e.target.value)
+                    }}
+                />
+           {
+               !isEmpty(error) &&  <Typography sx={{ fontSize: 11, color: 'red', mt: 0.7}}>{error}</Typography>
+           }
+
+           </div>
+
+            </FormControl>
+
             <TextField 
                 value={confirm}
                 label={'Confirm New Password'}
                 variant='outlined'
-                type={'password'}
+                type={show_password ? 'text': 'password'}
                 size='small'
                 fullWidth
-                sx={{ display: 'block', my: 1}}
+                required
+                sx={{ my: 1}}
                 onChange={(e) => {
                     setConfirm(e.target.value)
                 }}
@@ -66,6 +114,5 @@ export const AccountChangePassword = () => {
         {
             need_auth && <Button onClick={handleReauthenticate}>Reauthenticate</Button>
         }
-        <Button onClick={handleReauthenticate}>Reauthenticate</Button>
     </div>
 }
