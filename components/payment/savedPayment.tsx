aab5a478ea5af0229b7ecdd420357f1d3e634e9c
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-06-22 14:01:38 -0400

    change to radio group for save payment

--- components/payment/savedPayment.tsx
+++ components/payment/savedPayment.tsx
@@ -1,9 +1,9 @@
-import { Button, Card, CardContent, Radio, Typography } from '@mui/material';
+import { Button, Card, CardContent, FormControl, FormControlLabel, Radio, RadioGroup, Typography } from '@mui/material';
 import { blue } from '@mui/material/colors';
+import { Box } from '@mui/system';
 import { Stripe, StripeElements} from '@stripe/stripe-js'
-import { isEmpty, isString } from 'lodash';
+import { isEmpty } from 'lodash';
 import { FormEvent, useState } from 'react';
-import { FaCheck } from 'react-icons/fa';
 import { handleCatchError } from '../../utils/errors/custom';
 import { handlePayWithMethodId } from '../../utils/functions/payment';
 import { handleCreditCardBrand } from '../history/orderPayment';
@@ -21,17 +21,9 @@ interface ISavePaymentFormProps {
 
 export const SavedPaymentForm = ({ cards, cart, customer, stripe, elements, toggleForm }: ISavePaymentFormProps) => {
     // manage the initial state
-    const inititalCard:IPublicPaymentMethod = {
-        card: {
-            brand: '',
-            last_four: '',
-            exp_year: 0,
-            exp_month: 0,
-        },
-        id: ''
-    }
-    const [selectCard, setSelectedCard] = useState<IPublicPaymentMethod>(inititalCard);
-    const [isLoading, setIsLoading] = useState(false);
+      const [isLoading, setIsLoading] = useState(false);
+      const [value, setValue] = useState<string>('');
+
 
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         try {
@@ -39,10 +31,11 @@ export const SavedPaymentForm = ({ cards, cart, customer, stripe, elements, togg
 
             e.preventDefault();
             setIsLoading(true);
- 
-            // process the payment with a saved card
-            if(!isString(selectCard.id) || isEmpty(selectCard.id)){
-               throw new Error('Please select a card to proceed')
+
+            let found_card = cards.find(card => card.id === value);
+
+            if(!found_card){
+                throw new Error('Please select a card to proceed')
             }
 
             if(cart.is_delivery){
@@ -50,9 +43,9 @@ export const SavedPaymentForm = ({ cards, cart, customer, stripe, elements, togg
                     throw new Error('Please select tip for your driver')
                 }
             }
-
+           
             await handlePayWithMethodId({ 
-                card: selectCard, 
+                card: found_card, 
                 cart,
                 customer,
             });
@@ -66,21 +59,29 @@ export const SavedPaymentForm = ({ cards, cart, customer, stripe, elements, togg
     return <>
         <form id="payment_form" onSubmit={handleSubmit}>
             <Typography>Saved Card</Typography>
-            {
-                cards.map((card) => {
-                    return <Card key={card.id} sx={{ my: 2}} onClick={() => {
-                        setSelectedCard(card.id === selectCard.id ? inititalCard : card)
-                    }}>
-                    <CardContent sx={{ display: 'flex', alignItems: 'center', padding: '8px !important'}}> 
-                        <Radio />
-                        <div>{handleCreditCardBrand(card.card.brand)}</div>
-                        <Typography sx={{ mx: 2, fontWeight: 500}}>{card.card.brand === 'amex' ? `•••• •••••• •${card.card.last_four}` : `•••• •••• •••• ${card.card.last_four}`}</Typography>
-                        <Typography sx={{ fontWeight: 500}}>EXP:{card.card.exp_month} / {card.card.exp_year}</Typography>
-                           {/* {card.id === selectCard.id && <FaCheck />} */}
-                    </CardContent>
-                </Card>
-                })
-            }
+            <FormControl sx={{ width: '100%'}}>
+                <RadioGroup value={value} onChange={(e) => {
+                    setValue(e.target.value);               
+                }}>   
+                    {
+                        cards.map((card) => {
+                            return <Card key={card.id} sx={{ my:1}}>
+                            <CardContent sx={{ padding: '13px !important'}}> 
+                                <FormControlLabel value={card.id} control={<Radio />} sx={{ marginLeft: 0}} label={
+                                    <Box sx={{  display: 'flex', alignItems: 'center'}}>
+                                        <div style={{ marginLeft: '5px', display: 'flex', justifyContent: 'space-between'}}>{handleCreditCardBrand(card.card.brand)}</div>
+                                        <Typography sx={{ mx: 2, fontWeight: 500}}>{card.card.brand === 'amex' ? `•••• •••••• •${card.card.last_four}` : `•••• •••• •••• ${card.card.last_four}`}</Typography>
+                                        <Typography sx={{ fontWeight: 500}}>EXP:{card.card.exp_month} / {card.card.exp_year}</Typography>
+                                    </Box>
+                                } />             
+                            </CardContent>
+               
+                        </Card>
+                        })
+                    }
+                </RadioGroup>
+            </FormControl>
+           
 
             <TipSelection />
 
