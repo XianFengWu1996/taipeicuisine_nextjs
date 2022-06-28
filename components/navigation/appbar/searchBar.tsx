1773844ba34bbffec4af142ab7aa92254798463e
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-06-27 21:52:13 -0400

    add in option to display out of stock items

--- components/navigation/appbar/searchBar.tsx
+++ components/navigation/appbar/searchBar.tsx
@@ -1,13 +1,14 @@
-import { Button, Card, CardContent, Grid, InputBase, Paper, Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, Grid, InputBase, Paper, Typography } from "@mui/material";
 import { styled } from "@mui/system";
 import SearchIcon from '@mui/icons-material/Search';
 import { useState } from "react";
 import { blue, red } from "@mui/material/colors";
-import { useAppSelector } from "../../../store/store";
+import { useAppDispatch, useAppSelector } from "../../../store/store";
 import { isEmpty } from "lodash";
 import { handleCatchError } from "../../../utils/errors/custom";
 import { GoFlame } from "react-icons/go";
 import { HashLoader, MoonLoader } from "react-spinners";
+import { PublicMenuDialog } from "../../menu/publicMenuDialog";
 
 
 const Search = styled('div')(({ theme }) => ({
@@ -55,9 +56,13 @@ const Search = styled('div')(({ theme }) => ({
 export const SearchBar = () => {
     const [searchValue, setSearchValue] = useState<string>('');
     const [dish_result, setDishResult] = useState<IDish[]>([]);
+    const [out_of_stock, setOutOfStock] = useState<IDish[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState('');
     const { dishes } = useAppSelector(state => state.menus)
+
+    const [open, setOpen] = useState<boolean>(false);
+    const [selected, setSelected] = useState<IDish>({} as IDish);
 
     const handleOnSearch = () => {
       setError(''); 
@@ -69,16 +74,24 @@ export const SearchBar = () => {
           return setError('Please enter a search term...')
         }
         let temp_arr:IDish[] = [];
+        let temp_out_of_stock:IDish[] = [];
+
         dishes.forEach((dish) => {
           if(dish.en_name.toLowerCase().includes(searchValue.toLowerCase()) || dish.ch_name.toLowerCase().includes(searchValue.toLowerCase())){
-            temp_arr.push(dish);
+            if(dish.in_stock){
+              temp_arr.push(dish);
+            } else {
+              temp_out_of_stock.push(dish);
+            }
           }
         })
 
-        if(temp_arr.length === 0){
+        if(temp_arr.length === 0 && temp_out_of_stock.length === 0){
           return setError('No result was found...')
         }
+
         setDishResult(temp_arr);
+        setOutOfStock(temp_out_of_stock);
       } catch (error) {
         handleCatchError(error as Error, 'Failed to find the item')
       } finally {
@@ -114,23 +127,57 @@ export const SearchBar = () => {
         }
 
         {
-          !loading && <Grid container spacing={2} sx={{ pb: 20, px: 5, width: '100vw'}}>
-          {
-            !isEmpty(dish_result) && dish_result.map((dish) => {
-              return <Grid item xs={12} sm={6} md={6} lg={4} key={dish.id}>
-                <Card >
-                  <CardContent>
-                    <Typography sx={{ fontSize: 13, fontWeight: 600}}>{dish.label_id}.{dish.ch_name} {dish.is_spicy && <GoFlame color='red'/>}</Typography>
-                    <Typography sx={{ fontSize: 11, fontWeight: 500}}>{dish.en_name}</Typography>
-                    <Typography sx={{ fontSize: 12}}>${dish.price}</Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            })
-          }
-          </Grid>
+          !loading && <>
+            <Grid container spacing={2} sx={{ pb: 10, px: 5, width: '100vw'}}>
+            {
+              !isEmpty(dish_result) && dish_result.map((dish) => {
+                return dish.in_stock && <Grid item xs={12} sm={6} md={6} lg={4} key={dish.id}>
+                  <Card onClick={() => {
+                      console.log('clicked')
+                      setSelected(dish)
+                      setOpen(true);
+                    }}>
+                    <CardContent>
+                      <Typography sx={{ fontSize: 13, fontWeight: 600}}>{dish.label_id}.{dish.ch_name} {dish.is_spicy && <GoFlame color='red'/>}</Typography>
+                      <Typography sx={{ fontSize: 11, fontWeight: 500}}>{dish.en_name}</Typography>
+                      <Typography sx={{ fontSize: 12}}>${dish.price}</Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              })
+            }
+            </Grid>
+            
+            {
+              !isEmpty(out_of_stock) && <Typography>Out Of Stock Items </Typography>
+            }
+            <Grid container spacing={2} sx={{ pb: 20, px: 5, width: '100vw'}}>
+            {
+              !isEmpty(out_of_stock) && out_of_stock.map((dish) => {
+                return  <Grid item xs={12} sm={6} md={6} lg={4} key={dish.id}>
+                  <Card >
+                    <CardContent>
+                      <Typography sx={{ fontSize: 13, fontWeight: 600}}>{dish.label_id}.{dish.ch_name} {dish.is_spicy && <GoFlame color='red'/>}</Typography>
+                      <Typography sx={{ fontSize: 11, fontWeight: 500}}>{dish.en_name}</Typography>
+                      <Typography sx={{ fontSize: 12}}>${dish.price}</Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Typography sx={{ color: 'red', fontWeight: 500 }}>Currently Out of Stock</Typography>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              })
+            }
+            </Grid>
+          </>
         }
        
+
+       <PublicMenuDialog
+            open={open}
+            handleClose={() => setOpen(false)}
+            dish={selected}
+        />
        
 
 
