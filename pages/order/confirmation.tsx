65359a66fed3a3a91f6f9ab9abaf12c7fc20d1dd
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-07-07 14:51:15 -0400

    make confirmation page more mobile friendly

--- pages/order/confirmation.tsx
+++ pages/order/confirmation.tsx
@@ -1,25 +1,46 @@
 import { Button, Card, CardContent, Typography } from "@mui/material";
+import { styled } from "@mui/system";
 import Router, { useRouter } from "next/router";
 import { BsPatchCheck } from "react-icons/bs";
 
+export const ConfirmationTitle = styled(Typography)(({ theme }) => ({
+    fontSize: 50,
+    fontWeight: 500,
+    [theme.breakpoints.down('md')]: {
+        fontSize: 40,
+    },
+    [theme.breakpoints.down('sm')]: {
+        fontSize: 30,
+    }
+}))
+
+export const ConfirmationCardText = styled(Typography)(({ theme }) => ({
+    fontSize: 16,
+    lineHeight: 1.6,
+
+    [theme.breakpoints.down('sm')]: {
+        fontSize: 14,
+        fontWeight: 500,
+    }
+}))
 
 export default function Confirmation () {
     const query = useRouter().query as unknown as IOrderResult;
 
-    return <div style={{ height: '100vh'}}>
+    return <div style={{ height: '100vh', padding: '0 20px'}}>
         <div style={{ display: 'flex', flexDirection: 'column',justifyContent: 'center', alignItems:'center', height: 'inherit'}}>
             <div><BsPatchCheck size={100} color="#7FFFD4"/></div>
-            <Typography variant="h3">Order Confirmation</Typography>
+            <ConfirmationTitle>Order Confirmation</ConfirmationTitle>
 
-            <Card sx={{ my: 3, width: '500px'}}>
+            <Card sx={{ my: 3, maxWidth: '90%', minWidth: '300px'}}>
                 <CardContent style={{ display: 'flex', flexDirection: 'column', justifyContent: 'start'}}>
                     <Typography sx={{ fontWeight: 800}}>Order Summary</Typography>
-                    <Typography>Name: {query.name}</Typography>
-                    <Typography>Order #: {query.order_id}</Typography>
-                    <Typography>Number of Item(s): {query.item_count}</Typography>
-                    <Typography>Total: ${query.total}</Typography>
-                    <Typography>Place At: {query.order_time}</Typography>
-                    <Typography>Estimate Time: {query.estimate} minutes</Typography>
+                    <ConfirmationCardText>Name: {query.name}</ConfirmationCardText>
+                    <ConfirmationCardText>Order #: {query.order_id}</ConfirmationCardText>
+                    <ConfirmationCardText>Number of Item(s): {query.item_count}</ConfirmationCardText>
+                    <ConfirmationCardText>Total: ${query.total}</ConfirmationCardText>
+                    <ConfirmationCardText>Place On: {query.order_time}</ConfirmationCardText>
+                    <ConfirmationCardText>Estimate Time: {query.estimate} minutes</ConfirmationCardText>
                 </CardContent>
             </Card>
             <Typography>Your order has been placed. You will receive a email confirmation once the staff confirms the order. </Typography>
