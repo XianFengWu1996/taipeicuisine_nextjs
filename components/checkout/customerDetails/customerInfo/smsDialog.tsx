a822e8f0e924a07abefddca9ad57cae5bdb2aac1
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-07-08 01:55:05 -0400

    update sms to mobile friendly

--- components/checkout/customerDetails/customerInfo/smsDialog.tsx
+++ components/checkout/customerDetails/customerInfo/smsDialog.tsx
@@ -1,4 +1,5 @@
-import { Button, Dialog, DialogContent, TextField, Typography } from "@mui/material";
+import { Button, Dialog, DialogContent, TextField, Typography, useMediaQuery } from "@mui/material";
+import { styled } from "@mui/system";
 import { ChangeEvent, Dispatch, SetStateAction, useEffect, useState } from "react";
 import ReactCodeInput from 'react-verification-code-input';
 import { setShowSmsDialog } from "../../../../store/slice/settingSlice";
@@ -11,9 +12,20 @@ import { handleCodeVerify, sentCode } from "../../../../utils/functions/phone";
 // enable/disable the code input field base on the cookie
 // also, once the button is click, toggle to a button which sole job is to display the timer
 
+const PhoneInputContainer = styled('div')(({ theme }) => ({
+    display: 'flex', 
+    margin: '20px 0',
+
+    [theme.breakpoints.down('sm')]: {
+        flexDirection: 'column', 
+    }
+}))
+
 export const SmsDialog = () => {
     const { show_sms_dialog} = useAppSelector(state => state.setting)
     const dispatch = useAppDispatch();
+    const isMobile = useMediaQuery('(max-width: 720px)'); // check if it' mobile 
+
 
     const [smsPhone, setSmsPhone] = useState('');
     const [sent, setSent] = useState<boolean>(false); // this will handle the button and timer
@@ -41,25 +53,24 @@ export const SmsDialog = () => {
         <DialogContent>
             <Typography sx={{ my: 2}}>Add New Phone Number</Typography>
 
-            <div style={{ display: 'flex', margin: '20px 0'}}>
-           
-            <TextField
-                    id="customer_phone" 
-                    label="Phone"
-                    variant="outlined"
-                    fullWidth 
-                    type={'tel'}
-                    sx={{ letterSpacing: 2.5, flex: 5}}
-                    value={smsPhone}
-                    onChange={handlePhoneOnChange}
-            />
-            
-            <SmsVerificationButton phone={smsPhone} sent={sent} setSent={setSent}/>
+            <PhoneInputContainer>
+                <TextField
+                        id="customer_phone" 
+                        label="Phone"
+                        variant="outlined"
+                        fullWidth 
+                        type={'tel'}
+                        sx={{ letterSpacing: 2.5, flex: 5}}
+                        value={smsPhone}
+                        onChange={handlePhoneOnChange}
+                />
+                
+                <SmsVerificationButton phone={smsPhone} sent={sent} setSent={setSent}/>
         
-            </div>
+            </PhoneInputContainer>
 
             <Typography>Enter Verification Code</Typography>
-            <ReactCodeInput  onComplete={handleCodeVerify} /> 
+            <ReactCodeInput  onComplete={handleCodeVerify} fieldHeight={isMobile ? 40 : 60} fieldWidth={isMobile ? 40 : 60}/> 
         </DialogContent>
     </Dialog>
 }
@@ -71,6 +82,16 @@ interface ISmsVerificationButton {
 }
 
 export const SmsVerificationButton = ({phone, sent, setSent}: ISmsVerificationButton) => {
+    const SentButton = styled(Button)(({ theme }) => ({
+        flex: 3, 
+        marginLeft: 30,
+
+        [theme.breakpoints.down('sm')]:{
+            marginLeft: 0,
+            marginTop: 10
+        }
+    }))
+
     const [timer, setTimer] = useState(process.env.NEXT_PUBLIC_DEFAULT_TIMER);
 
     useEffect(() => {
@@ -94,8 +115,7 @@ export const SmsVerificationButton = ({phone, sent, setSent}: ISmsVerificationBu
 
     return <>
         {
-            !sent ? <Button 
-                    sx={{ flex: 3, marginLeft: "5%"}} 
+            !sent ? <SentButton 
                     variant="outlined"
                     onClick={() => {
                         sentCode({
@@ -103,13 +123,9 @@ export const SmsVerificationButton = ({phone, sent, setSent}: ISmsVerificationBu
                             handleStartLoading: () => setSent(true),
                         })
                     }}
-                >Send</Button> 
-                : <Button
-                    sx={{ flex: 3, marginLeft: "5%"}} 
-                    variant="outlined"
-                >
-                {timer}s
-            </Button>
+                >Send</SentButton> 
+                : <SentButton variant="outlined">{timer}s
+            </SentButton>
         }
     </>
 }
