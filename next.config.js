4e991ab2a10c04f62a7c545597fc32c1e3c6017c
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-07-09 02:11:26 -0400

    disable eslint on build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['media.istockphoto.com', 'firebasestorage.googleapis.com'],
+  },
+  eslint:{
+    ignoreDuringBuilds: true
   }
 }
 
